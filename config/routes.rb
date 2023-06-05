c1d3e3eca28ed3898fd108efc19817e1127631e9
mikhael zimmermann <mikhael928@gmail.com>
2023-06-06 01:20:10 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # Defines the root path route ("/")
   # root "articles#index"
-
-  root to: "pages#home"
   devise_for :users
-  resources :recommendations, only: %i[search index show]
+  root to: "pages#home"
+  get 'recommendations/search'
+  resources :recommendations, only: %i[index show]
 end

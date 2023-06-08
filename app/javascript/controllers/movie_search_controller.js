c13abc96bc0e9053541d35cb3ea950b682e92d18
mikhael zimmermann <mikhael928@gmail.com>
2023-06-08 17:22:54 +0200

    Finish logic of search bar

--- app/javascript/controllers/movie_search_controller.js
+++ app/javascript/controllers/movie_search_controller.js
@@ -2,10 +2,13 @@ import { Controller } from '@hotwired/stimulus';
 
 const omdbapiUrl = 'http://www.omdbapi.com/';
 const omdbapiKey = 'adf1f2d7';
+const numberCounters = ['one', 'two', 'three'];
+let counter = 0;
 
 const createMovieCard = (movie) => {
   return `
-  <div class="search-boxes-container-results-card">
+  <div class="search-boxes-container-results-card" data-action='click->movie-search#selectMovie'>
+    <input type="hidden" value="${movie.Title}">
     <img src="${movie.Poster}" class="search-boxes-container-results-card-image" />
     <p class="search-boxes-container-results-card-infos">
       ${movie.Title}
@@ -29,7 +32,17 @@ const createUrl = (search) => {
 
 // Connects to data-controller="movie-search"
 export default class extends Controller {
-  static targets = ['search', 'results', 'selected'];
+  static targets = [
+    'search',
+    'results',
+    'selected',
+    'recentOne',
+    'recentTwo',
+    'recentThree',
+    'selectedOne',
+    'selectedTwo',
+    'selectedThree',
+  ];
 
   connect() {
     this.resultsTarget.innerHTML = '';
@@ -39,8 +52,36 @@ export default class extends Controller {
     fetch(createUrl(event.target.value))
       .then((response) => response.json())
       .then((data) => {
-        if (data.Search) return (this.resultsTarget.innerHTML = createContent(data));
+        if (data.Search)
+          return (this.resultsTarget.innerHTML = createContent(data));
         this.resultsTarget.innerHTML = '';
       });
+  }
+
+  selectMovie(event) {
+    event.currentTarget.classList.add('selected');
+    counter += 1;
+
+    const image = event.currentTarget.querySelector('img').src;
+    const title = event.currentTarget.querySelector('p').innerHTML;
+    const dbTitle = event.currentTarget.querySelector('input').value;
+
+    this.recents = [this.recentOneTarget, this.recentTwoTarget, this.recentThreeTarget];
+    this.selecteds = [this.selectedOneTarget, this.selectedTwoTarget, this.selectedThreeTarget];
+
+    this.recents[counter - 1].value = dbTitle;
+    this.selecteds[counter - 1].querySelector('img').src = image;
+    this.selecteds[counter - 1].querySelector('p').innerHTML = title;
+  }
+
+  removeMovie(event) {
+    const selected = event.currentTarget;
+
+    const image = selected.querySelector('img');
+    const title = selected.querySelector('p');
+
+    counter -= 1;
+    image.src = '';
+    title.innerHTML = 'Select a movie';
   }
 }

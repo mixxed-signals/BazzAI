9cb239ce54efcc355d71d9310cb7a0da32d701d6
mikhael zimmermann <mikhael928@gmail.com>
2023-06-08 15:05:57 +0200

    Code to Kaodi

--- app/javascript/controllers/movie_search_controller.js
+++ app/javascript/controllers/movie_search_controller.js
@@ -1,23 +1,39 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
-const movieCard = (movie) => {
+const omdbapiUrl = 'http://www.omdbapi.com/';
+const omdbapiKey = 'adf1f2d7';
+
+const createMovieCard = (movie) => {
   return `
-  <div class="gray-box">
-    <img src="${movie.poster}" />
-    <p class="selected-movie">${movie.title} ( ${movie.year} )</p>
+  <div class="search-boxes-container-results-card">
+    <img src="${movie.Poster}" class="search-boxes-container-results-card-image" />
+    <p class="search-boxes-container-results-card-infos">${movie.Title} ( ${movie.Year} )</p>
   </div>
-  `
-}
+  `;
+};
+
+const createContent = (data) => {
+  return data.Search.map((movie) => createMovieCard(movie)).join('');
+};
+
+const createUrl = (search) => {
+  return `${omdbapiUrl}?apikey=${omdbapiKey}&s=${search}`;
+};
 
 // Connects to data-controller="movie-search"
 export default class extends Controller {
-  static targets = [ "search" , "results" , "selected" ]
+  static targets = ['search', 'results', 'selected'];
 
   connect() {
-    console.log("Search controller connected");
+    console.log('Search controller connected');
   }
 
   handleInput(event) {
-    fetch(``).then(response => response.json()).then((data) => {console.log(data)})
+    fetch(createUrl(event.target.value))
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.Search) return this.resultsTarget.innerHTML = createContent(data);
+        this.resultsTarget.innerHTML = '';
+      });
   }
 }

12ed9e69e32fa598ea816c98ef4236181a78be7d
mikhael zimmermann <mikhael928@gmail.com>
2023-06-08 14:46:50 +0200

    Start search bar inside search page

--- app/javascript/controllers/movie_search_controller.js
+++ app/javascript/controllers/movie_search_controller.js
@@ -1,43 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 
+const movieCard = (movie) => {
+  return `
+  <div class="gray-box">
+    <img src="${movie.poster}" />
+    <p class="selected-movie">${movie.title} ( ${movie.year} )</p>
+  </div>
+  `
+}
+
 // Connects to data-controller="movie-search"
 export default class extends Controller {
+  static targets = [ "search" , "results" , "selected" ]
+
   connect() {
     console.log("Search controller connected");
   }
 
   handleInput(event) {
-    this.search();
+    fetch(``).then(response => response.json()).then((data) => {console.log(data)})
   }
 }
-
-const omdbapiUrl = "http://www.omdbapi.com/";
-const imgUrl = `http://img.omdbapi.com/?apikey=${apiKey}&`;
-const searchForm = document.querySelector("#movie-name");
-const movieDisplay = document.querySelector("#movie-cards");
-
-const searchMovies = () => {
-  // event.preventDefault();
-  const title = document.querySelector("#movie-name").value;
-  movieDisplay.innerHTML = "";
-  const url = `${omdbapiUrl}?s=${encodeURIComponent(title)}&apikey=${apiKey}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      if (data.Search) {
-        const newList = data.Search.map((movie) => {
-          if (movie.Poster === "N/A") {
-            return `<div class="col-lg-3 col-md-4 col-sm-6 col-12"><div class="card mb-2"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png" class="card-img-top" alt="${movie.Title}"><div class="card-body"><span class="badge bg-primary mb-2">${movie.Year}</span><h5 class="card-title">${movie.Title}</h5></div></div></div>`;
-          }
-          return `<div class="col-lg-3 col-md-4 col-sm-6 col-12"><div class="card mb-2"><img src="${movie.Poster}" class="card-img-top" alt="${movie.Title}"><div class="card-body"><span class="badge bg-primary mb-2">${movie.Year}</span><h5 class="card-title">${movie.Title}</h5></div></div></div>`;
-        }).join("");
-        movieDisplay.innerHTML = newList;
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
-
-searchForm.addEventListener("keyup", searchMovies);

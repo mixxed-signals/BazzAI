9b518eb20d69701f93008a2270abcb50b002580d
kaodi alum <kaodialum@yahoo.com>
2023-06-12 12:55:57 +0200

    adding logos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_09_110420) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_081909) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_09_110420) do
     t.integer "year_after"
     t.integer "year_option"
     t.bigint "user_id", null: false
+    t.string "streaming_platform"
     t.index ["user_id"], name: "index_queries_on_user_id"
   end
 

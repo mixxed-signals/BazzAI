d9d393df6382c4757aa06e3cc89a483880a8f22a
Niklas Springer <me@niklaspringer.xyz>
2023-06-15 12:14:23 +0200

    min

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,4 +1,4 @@
-import { Controller } from "@hotwired/stimulus";
+import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
   static targets = ["value", "output", "input"];
@@ -8,6 +8,12 @@ export default class extends Controller {
   }
 
   updateValue() {
+    const sliderValue = this.inputTarget.value;
+    const outputTarget = this.outputTarget;
+    outputTarget.textContent = sliderValue;
+  }
+
+  updateMinValue() {
     const sliderValue = this.inputTarget.value;
     const outputTarget = this.outputTarget;
 
